f092afc81af8f78f47fc7c156c601d4785e81781
gintare.mazeikyte <gintare.mazeikyte@zenitech.co.uk>
2022-07-05 18:27:42 +0300

    Add new test for shoping card

---
+++ Baigiamasis darbas Mathscientific/Pages/ShoppingCardPage.cs
@@ -0,0 +1,43 @@
+﻿namespace Baigiamasis_darbas_Mathscientific.Pages
+{
+    public class ShoppingCardPage
+    {
+        public static void moveMouseOnMaistoPapildai()
+        {
+            string locator = "//*[@id='menu-item-62457']/a";
+            Common.performHoverMouse(locator);
+        }
+
+        public static void cklickOnKolagenasFromTheList()
+        {
+            string locator = "//*[@id='menu-item-62473']";
+            Common.clickElement(locator);
+        }
+
+        public static void cklickOnTheKolagenasSuAvietemis()
+        {
+            string locator = "//*[text()='Grynas kolagenas su avietėmis']";
+            Common.clickElement(locator);
+        }
+
+        public static string readMessageInBasket()
+        {
+                //string locator = "//*[@class='add-to-cart__wrapper']//span";
+                //string locator = "//*[@class='added-to-cart-msg__text' and text()='Produktas sėkmingai įdėtas į krepšelį.']";
+                string locator = "//*[text()='Produktas sėkmingai įdėtas į krepšelį.']";
+                Common.waitForElementToBeVisible(locator);
+                //*[contains(text(),'Produktas sėkmingai įdėtas į krepšelį')]";
+                //*[@class='added-to-cart-msg__text']";
+                //*[contains(text(),'Produktas sėkmingai įdėtas į krepšelį')]
+                //span[@class='added-to-cart-msg__text']"
+
+                return Common.getElementText(locator);
+        }
+
+        public static void addProductSuAvietemisToShoppCard()
+        {
+            string locator = "//*[@data-name='Grynas kolagenas su avietėmis']";
+            Common.clickElement(locator);
+        }
+    }
+}
---
+++ Tests/ShoppingCard.cs
@@ -0,0 +1,30 @@
+﻿using Baigiamasis_darbas_Mathscientific.Pages;
+using NUnit.Framework;
+using Tests.BaseClasses;
+
+namespace Tests
+{
+    public class ShoppingCard : Basetests
+    {
+        [SetUp]
+        public static void openPage()
+        {
+            HomePage.open();
+            //HomePage.closeCokies();
+        }
+
+        [Test]
+        public static void addToCard()
+        {
+            string expectedMessage = "Produktas sėkmingai įdėtas į krepšelį.";
+
+            HomePage.moveMouseOnMenu();
+            ShoppingCardPage.moveMouseOnMaistoPapildai();
+            ShoppingCardPage.cklickOnKolagenasFromTheList();
+            ShoppingCardPage.cklickOnTheKolagenasSuAvietemis();
+            ShoppingCardPage.addProductSuAvietemisToShoppCard();
+            string actualMessage = ShoppingCardPage.readMessageInBasket();
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+    }
+}
