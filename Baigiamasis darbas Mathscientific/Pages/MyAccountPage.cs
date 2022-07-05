4d29056f7e113303a83576fabe959d3f940a2a14
gintare.mazeikyte <gintare.mazeikyte@zenitech.co.uk>
2022-07-05 17:16:24 +0300

    Add new classes in Page in Tests for MyAccout

---
+++ Baigiamasis darbas Mathscientific/Pages/MyAccountPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baigiamasis_darbas_Mathscientific.Pages
+{
+    public class MyAccountPage
+    {
+        public static void navigateToRegistrationPage()
+        {
+            string locator = "//*[@class='navbar__user-area__login']";
+            Common.clickElement(locator);
+        }
+
+        public static void enterEmail(string name)
+        {
+            string locator = "//*[@id='reg_email']";
+            Common.sendKeysToElement(locator, name);
+        }
+
+        public static void enterPassword(string name)
+        {
+            string locator = "//*[@id='reg_password']";
+            Common.sendKeysToElement(locator, name);
+        }
+
+        public static void clickRegistrationButton()
+        {
+            string locator = "//*[@value='Registruotis']";
+            Common.clickElement(locator);
+        }
+
+        public static string readErrorMessage()
+        {
+            string locator = "//*[@id='content']//*[text()=' Paskyra su tokiu el.pašto adresu jau yra sukurta. ']";
+            //*[@id="content"]/div/div/div[1]/ul/li/text()
+            return Common.getElementText(locator);
+        }
+    }
+}
---
+++ Tests/MyAccount.cs
@@ -0,0 +1,32 @@
+﻿using Baigiamasis_darbas_Mathscientific.Pages;
+using NUnit.Framework;
+using Tests.BaseClasses;
+
+namespace Tests
+{
+    public class MyAccount : Basetests
+    {
+        [SetUp]
+        public static void openPage()
+        {
+            HomePage.open();
+
+            //HomePage.clickCloseSuscriberButtonWhichEnabledAfter15sec();
+            //HomePage.closeCokiesWithButtonOnlyEssentials();
+        }
+
+        [Test]
+        public static void createAccountWithAlreadyExistingCredentials()
+        {
+            string email = "saule@saule.lt";
+            string password = "KatinasSuo01!";
+            string expectedMessage = "Klaida: Paskyra su tokiu el.pašto adresu jau yra sukurta. Prisijunkite.";
+            MyAccountPage.navigateToRegistrationPage();
+            MyAccountPage.enterEmail(email);
+            MyAccountPage.enterPassword(password);
+            MyAccountPage.clickRegistrationButton();
+            string actualMessage = MyAccountPage.readErrorMessage();
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+    }
+}
